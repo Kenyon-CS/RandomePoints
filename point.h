d615684829c5ff1b8322109fef06301e7e0862e3
James Skön <skonjp@kenyon.edu>
2024-09-04 16:52:57 -0400

    Update point.h

--- point.h
+++ point.h
@@ -1,39 +1,35 @@
-// Point.cpp
-#include "Point.h"
-#include <cmath>
+// Point.h
+#ifndef POINT_H
+#define POINT_H
 
-// Default constructor
-Point::Point() : x(0.0), y(0.0) {}
+class Point {
+private:
+    double x, y;
 
-// Parameterized constructor
-Point::Point(double xVal, double yVal) : x(xVal), y(yVal) {}
+public:
+    // Default constructor
+    Point();
 
-// Equality operator overload
-bool Point::operator==(const Point& other) const {
-    return (x == other.x) && (y == other.y);
-}
+    // Parameterized constructor
+    Point(double xVal, double yVal);
 
-// Subtraction operator overload to calculate distance between points
-double Point::operator-(const Point& other) const {
-    return std::sqrt(std::pow(x - other.x, 2) + std::pow(y - other.y, 2));
-}
+    // Equality operator overload
+    bool operator==(const Point& other) const;
 
-// Getter for x coordinate
-double Point::GetX() const {
-    return x;
-}
+    // Subtraction operator overload to calculate distance between points
+    double operator-(const Point& other) const;
 
-// Getter for y coordinate
-double Point::GetY() const {
-    return y;
-}
+    // Getter for x coordinate
+    double GetX() const;
 
-// Setter for x coordinate
-void Point::SetX(double xVal) {
-    x = xVal;
-}
+    // Getter for y coordinate
+    double GetY() const;
 
-// Setter for y coordinate
-void Point::SetY(double yVal) {
-    y = yVal;
-}
+    // Setter for x coordinate
+    void SetX(double xVal);
+
+    // Setter for y coordinate
+    void SetY(double yVal);
+};
+
+#endif
